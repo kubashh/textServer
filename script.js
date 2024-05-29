9c77e809423face6bd48a733af018dc44d9dbd16
kubashh <146523311+kubashh@users.noreply.github.com>
2024-05-29 16:44:22 +0000

    1.1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const element = document.getElementById("element");
 
 element.textContent = "siema";
+const socket = io("http://192.168.0.167:3000/");
+
+socket.on("connect", () => {
+  element.innerHTML = "";
+  element.textContent += "works";
+});
+
+socket.on("text", (text) => {
+  element.textContent += text;
+});
